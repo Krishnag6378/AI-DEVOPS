e50cd2529dd6773502e3906edab5e162610828ed
Krishna Gupta <krishnagupta6378@gmail.com>
2026-02-07 18:44:17 +0530

    Refactor agent.py to streamline input handling

--- aiagents/agent.py
+++ aiagents/agent.py
@@ -2,12 +2,12 @@ from strands import Agent
 from strands.models.ollama import OllamaModel
 from strands_tools import http_request
 
-
 model = OllamaModel(
     host="http://localhost:11434",
-    model="qwen2.5:1.5b",   
+    model="qwen2.5:1.5b",
     temperature=0.7,
 )
+
 system_prompt = """
 You are an AI agent.
 You may use tools when needed.
@@ -20,11 +20,6 @@ agent = Agent(
     tools=[http_request],
 )
 
-
-while True:
-    user_input = input("\nYOU: ")
-    if user_input.lower() in ["exit", "quit"]:
-        break
-
-    response = agent(user_input)
-    print("\nAGENT:", response)
+user_input = input("YOU: ")
+response = agent(user_input)
+print("AGENT:", response)
